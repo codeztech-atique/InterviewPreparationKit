13a04654d47b039fba0813f3a086a31d9482b3e6
Atique Ahmed <atique.ahmed@asianetnews.in>
2020-09-23 21:38:55 +0530

    bit o

--- Big O/TimeComplexity/O n log n/index.js
+++ Big O/TimeComplexity/O n log n/index.js
@@ -2,7 +2,7 @@ var n = 10;
 
 function TimeComplexity_DoubleForLoop() {
     for(var i=0;i<=n;i++) {  //This we know O(n)
-        for(var j=1;j<=n;j=j*2) { // This is we know O(logN base 2) times
+        for(var j=1;j<=n;j=j/2) { // This is we know O(logN base 2) times
             console.log(i," ------> ", j); //O(logN) times
         }
     } //O(n^2)
