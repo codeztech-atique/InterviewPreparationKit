af42f515e44fe8f8c2df0314fcfd414a3fa88ca3
Atique Ahmed <atique.ahmed@zoom.us>
2024-04-21 00:28:17 +0530

    updated code

--- callback.js
+++ callback.js
@@ -1,11 +1,10 @@
 var myCallback = function(data) {
  console.log('got data: '+data);
- // Push data
 };
   
 var usingItNow = function(callback) {
    callback('get it?');
    // Getting/Printing the data from array
 };
-  
+
 usingItNow(myCallback)
