6dc3018b833370e0c99c8e000b50b6b91086e9e4
Atique Ahmed <atique1224@gmail.com>
2025-10-05 19:16:13 +0530

    Complete backtrack

---
+++ Datastructure/Algorithms/Backtracking/combinationSum.js
@@ -0,0 +1,26 @@
+// Combination sum to reuse the same value;
+
+function combinationSum(nums, target) {
+  nums.sort((a, b) => a - b);       // sort is optional here
+  const result = [];
+
+  function backtrack(index, current, remain) {
+    if (remain === 0) { result.push([...current]); return; }
+    if (remain < 0 || index === nums.length) return;
+
+    // EXCLUDE current number -> move to next index
+    backtrack(index + 1, current, remain);
+
+    // INCLUDE current number -> keep index (allow reuse)
+    current.push(nums[index]);
+    backtrack(index, current, remain - nums[index]); // index stays the same
+    current.pop();
+  }
+
+  backtrack(0, [], target);
+  return result;
+}
+
+// Example
+console.log(combinationSum([2,3,6,7], 7)); // [[7],[2,2,3]]
+
--- Datastructure/Algorithms/Backtracking/practice.js
+++ Datastructure/Algorithms/Backtracking/practice.js
@@ -1,10 +1,56 @@
 function generateBinaryStrings(n) {
+    let result = [];
+    let backtrack = (current) => {
+        if(current.length == n) {
+            result.push(current);
+            return;
+        }
 
+        backtrack(current + "0");
+        backtrack(current + "1");
+    }
+    backtrack("");
+    return result;
 }
 
 function generateSubsetBacktrack(n) {
+    let result = [];
+    let backtrack = (index, current) => {
+        if(index == n.length) {
+            result.push([...current]);
+            return;
+        }
 
+        backtrack(index + 1, current);
+        current.push(n[index]);
+
+        backtrack(index + 1, current);
+        current.pop();
+    }
+
+    backtrack(0, []);
+    return result;
+}
+
+function combinationSum(arr, target) {
+    let result = [];
+    let backtrack = (index, current, remain) => {
+        if(remain == 0) {
+            result.push([...current]);
+            return;
+        }
+        if(remain < 0 || index == arr.length) return;
+
+        backtrack(index + 1, current, remain);
+        current.push(arr[index]);
+
+        backtrack(index, current, remain - arr[index]);
+        current.pop();
+    }
+    backtrack(0, [], target);
+    return result;
 }
 
 console.log(generateSubsetBacktrack([1,2,3,4]));
 console.log(generateBinaryStrings(2));
+console.log(combinationSum([2,3,6,7], 7)); // [[7],[2,2,3]]
